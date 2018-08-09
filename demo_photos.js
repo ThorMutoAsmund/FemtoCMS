378e7e51fcc53b5311767a5737494ff13e3ed3ad
Thor Muto Asmund <thorasmund@gmail.com>
2018-08-09 14:06:30 +0200

    Add files via upload

--- demo_photos.js
+++ demo_photos.js
@@ -15,10 +15,10 @@ class DemoPhotos {
 
     firebase.initializeApp(this.firebaseConfig);
 
-    this.galleryIdsPromise = this.loadGalleryIds();
+    this.loadGalleriesPromise = this.loadGalleries();
   }
 
-  loadGalleryIds() {
+  loadGalleries() {
     return new Promise((resolve) => {
       firebase.database().ref('/galleries').once('value').then(snapshot => {
         resolve(snapshot);
@@ -26,25 +26,53 @@ class DemoPhotos {
     });
   }
 
-  getPageAsync(pageId = null) {
+  getPageAsync(pageId = null, parameters = []) {
     // Return only page IDs
     if (pageId == null) {
       return Promise.resolve(['page_photos']);
     }
 
+    // If unsupported page requested, reject promise
     if (pageId != 'page_photos') {
       return Promise.reject();
     }
 
-    return new Promise((resolve) => {
-      this.galleryIdsPromise.then(galleryIds => {
+    return new Promise((resolve, reject) => {
+      this.loadGalleriesPromise.then(galleries => {
         var result = '';
-        galleryIds.forEach(gallery => {
-          result += '<div style="padding:40px; border:1px solid black; margin:10px"><span style="font-size:48px">'+gallery.key+'</span></div>';
-        });
-
-        result = '<h2>Photo Gallery</h2><div style="display: flex">' + result + '</div>';
-        resolve(result);
+        if (parameters.length > 0) {
+          var gallery = galleries.child(parameters[0]).val();          
+          if (gallery) {
+            var heading ='<h2>' + (gallery.name || 'untitled') + '</h2>';
+            if (gallery.images) {
+              gallery.images.forEach(image => {
+                result += '<div style="padding:8px; border:1px solid black; margin:10px"><a href="#photos/'+gallery.key+'/'+image.key+'">';
+                result += '<img src="'+image.path+'" width="200" />';
+                if (image.title) {
+                  result += '<p>'+image.title+'</p>';
+                }
+                result += '</a></div>';
+              });
+              result = heading + '<div style="display: flex">' + result + '</div>';
+            }
+            else {
+              result = '<p>No images</p>';
+            }
+            resolve(result);
+          }
+          else {
+            reject();
+          }
+        }
+        else {
+          galleries.forEach(gallery => {
+            var data = gallery.val();
+            result += '<div style="padding:40px; border:1px solid black; margin:10px"><a href="#photos/'+gallery.key+'"><span style="font-size:48px">'+data.name+'</span></a></div>';
+          });
+  
+          result = '<h2>Photo Gallery</h2><div style="display: flex">' + result + '</div>';
+          resolve(result);
+        }
       });
     });
   }
